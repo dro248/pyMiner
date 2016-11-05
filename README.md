e5126c8dc806d31db25d531fbdca6fd5adc53d5c
David <david.ostler001@gmail.com>
2016-11-05 13:09:48 -0600

    A minimum working draft

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ To import them directly from the file, use:
 ```
 pip install -r requirements.txt
 ```
+
+##Usage
+```
+./miner.py NUMBER_OF_SEARCHES EMAIL_ADDRESS PASSWORD
+```
+
 ##Development
 In order to keep the *requirements.txt* dependencies file up-to-date, if a new package is required, use:
 ```
--- miner.py
+++ miner.py
@@ -5,14 +5,35 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import *
 import random, sys
+import time
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    words = [line.strip() for line in open("wordsenglish.txt")]
+    EMAIL = argv[2]
+    PASSWORD = argv[3]
+
+    # Dependencies
     chromeDriverLocation = "./chromedriver"
     driver = webdriver.Chrome(chromeDriverLocation)
+    words = [line.strip() for line in open("wordsenglish.txt")] # delete later
+
+    # login
+    driver.get("https://login.live.com/")
+    emailbox = driver.find_element_by_id("i0116")
+    emailbox.send_keys(EMAIL)
+    emailbox.send_keys(Keys.RETURN)
+
+    # wait for next textbox to show up
+    time.sleep(5)
+    passwordbox = driver.find_element_by_id("i0118")
+    passwordbox.send_keys(PASSWORD)
+    passwordbox.send_keys(Keys.RETURN)
+
+    time.sleep(5)
+
     driver.get("http://www.bing.com")
 
     for i in range(0,int(argv[1])):
@@ -20,6 +41,9 @@ def main(argv=None):
         search_box.clear()
         search_box.send_keys(random.choice(words))
         search_box.send_keys(Keys.RETURN)
+
+        # wait a few seconds
+        time.sleep(3)
 
 if __name__ == "__main__":
     main()
